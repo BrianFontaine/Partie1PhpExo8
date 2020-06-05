b83412ee4d327ecea3e36c7f4106528e52d15a5b
Brian Fontaine <brian02440@live.fr>
2020-06-05 12:26:41 +0200

    exo8

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    include'../header.php';
+?>
+<body>
+    <h1>Exercices 8</h1>
+    <p><?=$number1 = 3+4;$number1;?><p>
+    <p><?=$number2 = 5*20;$number2;?><p>
+    <p><?=$number3 = 45/5;$number3;?><p>
+</body>
+<?php
+    include'../footer.php';
+?>
